cf5e5fe64b81b624a62b4fe34fc87b1c76fac17b
ITALO COSTA RIBEIRO <italocosta019641@gmail.com>
2026-05-06 22:57:17 -0300

    ajuste 2.1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,79 +7,85 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        //Objetos
-        Scanner sc = new Scanner(System.in);
-        Aluno aluno = new Aluno();
-        Connection con = Conexao.getConnection();
-        AlunoDAO dados = new AlunoDAO(con);
-
-        //Interação, Cadastro
-        System.out.print("Digite o nome do aluno: ");
-        aluno.setNome(sc.nextLine());
-
-        System.out.print("Digite o ano de nascimento (AAAA-MM-DD): ");
-        aluno.setDataNascimento(LocalDate.parse(sc.nextLine()));
-
-        System.out.print("Digite a serie (Apenas o numero que a represente): ");
-        aluno.setSerie(sc.nextLine());
-
+         //Objetos
+         Scanner sc = new Scanner(System.in);
+         Aluno aluno = new Aluno();
+         Connection con = Conexao.getConnection();
+         AlunoDAO dados = new AlunoDAO(con);
         while (true) {
-            System.out.print("Digite a turma > A, B, C ou D: ");
-            aluno.setTurma(sc.nextLine().toUpperCase());
-            if (aluno.getTurma().equals("A") || aluno.getTurma().equals("B") || aluno.getTurma().equals("C") || aluno.getTurma().equals("D")) {
-                break;
-            } else{
-                System.out.println("Digite apenas: A, B, C ou D !! ");
-            }
+            System.out.println("Selecione uma opção\n" +
+                    "1 - Cadastrar dados\n" +
+                    "2 - Consultar dados\n" +
+                    "3 - Editar dados\n" +
+                    "4 - Deletar dados\n" +
+                    "5 - Fecha ");
+            int opcao = sc.nextInt();
+            sc.nextLine();
+            switch (opcao){
+                case 1:
+                    //Interação, Cadastro
+                    System.out.print("Digite o nome do aluno: ");
+                    aluno.setNome(sc.nextLine());
 
-        }
-        dados.salvar(aluno);
+                    System.out.print("Digite o ano de nascimento (AAAA-MM-DD): ");
+                    aluno.setDataNascimento(LocalDate.parse(sc.nextLine()));
 
-        //Visualização dos registros do banco de dados
-        while(true) {
-            System.out.println("Consultar Dados: 1 - sim  2 - não");
-            String res = sc.nextLine();
-            if(res.equals("1")){
-                for (Aluno a : dados.dadosDao()) {
+                    System.out.print("Digite a serie (Apenas o numero que a represente): ");
+                    aluno.setSerie(sc.nextLine());
+
+                    while (true) {
+                        System.out.print("Digite a turma > A, B, C ou D: ");
+                        aluno.setTurma(sc.nextLine().toUpperCase());
+                        if (aluno.getTurma().equals("A") || aluno.getTurma().equals("B") || aluno.getTurma().equals("C") || aluno.getTurma().equals("D")) {
+                            break;
+                        } else {
+                            System.out.println("Digite apenas: A, B, C ou D !! ");
+                        }
+                    }
+                    dados.salvar(aluno);
+                    break;
+                case 2:
+                    //Visualização dos registros do banco de dados
+                    for (Aluno a : dados.dadosDao()) {
                     System.out.println("Nome " + a.getNome());
                     System.out.println("Idade " + a.getIdade());
                     System.out.println("Serie " + a.getSerie() + "° Ano");
                     System.out.println("Turma " + a.getTurma());
                     System.out.println("ID " + a.getId());
-                }
-            }else {
+                    }
                 break;
-            }
-        }
-        //Atualizar registros
-        while (true){
-            System.out.println("Atualizar registro: 1 - sim  2 - não");
-            String res = sc.nextLine();
-            if (res.equals("1")) {
-                System.out.println("ID: ");
-                int id = sc.nextInt();
-                sc.nextLine();
-                aluno.setId(id);
-                System.out.println("Atualizar nome: ");;
-                String nome = sc.nextLine();
-                aluno.setNome(nome);
-                System.out.println("Atualizar turma: ");
-                String turma = sc.nextLine();
-                aluno.setTurma(turma);
-                System.out.println("Atualizar serie: ");
-                String serie = sc.nextLine();
-                aluno.setSerie(serie);
 
-                dados.update(aluno);
-            } else{
+                case 3:
+                    //Atualizar registros
+                    System.out.println("ID: ");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+                    aluno.setId(id);
+                    System.out.println("Atualizar nome: ");
+                    String nome = sc.nextLine();
+                    aluno.setNome(nome);
+                    System.out.println("Atualizar turma: ");
+                    String turma = sc.nextLine();
+                    aluno.setTurma(turma);
+                    System.out.println("Atualizar serie: ");
+                    String serie = sc.nextLine();
+                    aluno.setSerie(serie);
+
+                    dados.update(aluno);
                 break;
+
+                case 4:
+
+                break;
+
+                case 5:
+                    try {
+                        if (con != null) con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                return;
             }
-        }
-        try {
-            if (con != null) con.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
